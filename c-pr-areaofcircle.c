fd379f8110f37b373fc978c3172f544c3e84178d
Jeswin Christie <121104372+NIGHTFURY609@users.noreply.github.com>
2025-05-02 18:07:30 +0000

    adding a practice code

---
+++ c-pr-areaofcircle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+#define PI 3.14
+// Symbolic constant
+
+void main( )
+
+{
+
+float radius,area;
+
+clrscr();
+/* variable declarations */
+
+printf (“radius =”);
+/ * output statement (prompt) /*
+
+scanf (“%f”, &radius) ;
+/* input statement * /
+
+area = PI* radius * radius;
+/* assignment statement */
+
+printf ("area = %f",area) ;
+
+getch();
+
+}
