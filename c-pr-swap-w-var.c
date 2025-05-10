e27b19311f7dcefeacf985e71ee3dbd9d0d86091
Jeswin Christie <121104372+NIGHTFURY609@users.noreply.github.com>
2025-05-10 04:28:02 +0000

    c programming practicce xor swap

--- c-pr-swap-w-var.c
+++ c-pr-swap-w-var.c
@@ -1,12 +1,14 @@
+//it uses XOR bit operation now
+
 #include <stdio.h>
 
 int main() {
     int a = 5, b = 10;
     printf("Before swap: a = %d, b = %d\n", a, b);
 
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    a ^= b;
+    b ^= a;
+    a ^= b;
 
     printf("After swap: a = %d, b = %d\n", a, b);
     return 0;
